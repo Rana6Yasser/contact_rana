d12834e179d3d03c081e436433272282a9bca6e4
Rana6Yasser <ranayasser740@gmail.com>
2026-07-20 23:26:23 +0000

    make some changes

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,36 @@
-
-
 function upDate(previewPic){
-    
-    document.getElementById("image").style.backgroundImage = "url('" + previewPic.src + "')";
 
-    document.getElementById("image").innerHTML = previewPic.alt;
+    console.log("Mouse Over or Focus");
+
+    let imageDiv = document.getElementById("image");
+
+    imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
+
+    imageDiv.innerHTML = previewPic.alt;
 }
 
 function unDo(){
-    document.getElementById("image").style.backgroundImage = "url('')";
 
-    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+    console.log("Mouse Leave or Blur");
+
+    let imageDiv = document.getElementById("image");
+
+    imageDiv.style.backgroundImage = "url('')";
+
+    imageDiv.innerHTML = "Hover over an image below to display here.";
+}
+
+function addTabIndex(){
+
+    console.log("Page Loaded");
+
+    let images = document.querySelectorAll(".preview");
+
+    for(let i = 0; i < images.length; i++){
+
+        images[i].setAttribute("tabindex","0");
+
+        console.log("Tabindex added to image " + (i+1));
+    }
+
 }
